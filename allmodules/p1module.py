3b7b3003a84b038ce4af7ffdd7a6bfd77f26b701
muchcreative <robby.j.lem@hotmail.com>
2022-04-20 19:35:37 -0600

    Updated P1 module docstrings and formatting.

--- allmodules/p1module.py
+++ allmodules/p1module.py
@@ -1,31 +1,58 @@
+'''Part 1 Tutorial Modules.
+
+Functions:
+  get_sp500_constituents_records(filepath)
+    Gets SP500 constituents records from the specified filepath.
+
+  format_sp500_constituents_records(df)
+    Formats sp500 constituents records by splitting up individual tickers and changing date strings to datetimes.
+
+  slice_sp500_constituents_records(df, start_date, end_date)
+    Slices sp500 constituents records to specified start and end dates.
+
+  collect_all_sp500_constituents(df)
+    Returns an alphabetically sorted list of all constituents that were in the sp500.
+'''
+
 import pandas as pd
 import datetime as dt
 
 from pathlib import Path
-import json
 
-def get_sp500_constitutents_records(filepath):
-    sp500_constituents_file = Path(filepath)
-    if sp500_constituents_file.is_file():
-      df = pd.read_csv(filepath, index_col='date')
-    else:
-      return print('Could not find SP500 Constituents Records')
+def get_sp500_constituents_records(filepath):
+  '''Gets SP500 constituents records from the specified filepath.
+
+  Args:
+    filepath: string of where the sp500 constituents records is saved.
+      
+  Returns:
+    sp500_constituents_records: pandas dataframe. If the filepath does not point 
+                                to the file location; None will be returned.
+  '''
+
+  sp500_constituents_file = Path(filepath)
+  if sp500_constituents_file.is_file():
+    sp500_constituents_records = pd.read_csv(filepath, index_col='date')
+    return sp500_constituents_records
+  else:
+    print('Could not find SP500 Constituents Records')
+  return
 
 def format_sp500_constituents_records(df):
-  df['tickers'] = df['tickers'].apply(lambda x: sorted(x.split(','))) #Change each ticker row to lists
-  df.index = pd.to_datetime(df.index, format = '%Y-%m-%d') #Change Date (str) Index to a Datetime Index
+  '''Formats sp500 constituents records by splitting up individual tickers and changing date strings to datetimes.'''
+  df['tickers'] = df['tickers'].apply(lambda x: sorted(x.split(',')))  # Change each ticker row to lists.
+  df.index = pd.to_datetime(df.index, format = '%Y-%m-%d')  # Change date (str) index to a datetime index.
   return df
 
-def slice_sp500_constituents_records(df,
-                                     start_date,
-                                     end_date):
-  start_date = dt.datetime.strptime(start_date,'%Y-%m-%d') #Change string dates to datetime for pandas to compare them
-  end_date = dt.datetime.strptime(end_date,'%Y-%m-%d')
+def slice_sp500_constituents_records(df, start_date, end_date):
+  '''Slices sp500 constituents records to specified start and end dates.'''
+  start_date = dt.datetime.strptime(start_date, '%Y-%m-%d')  # Change string dates to datetime for pandas to compare them.
+  end_date = dt.datetime.strptime(end_date, '%Y-%m-%d')
   date_ranged_df = df.loc[start_date:end_date]
   return date_ranged_df
 
 def collect_all_sp500_constituents(df):
-  '''Returns an alphabetically sorted list of all constituents that were in the sp500 for the sliced date_range df'''
+  '''Returns an alphabetically sorted list of all constituents that were in the sp500.'''
   sp500_constituents = set()
   for years_constituents in df['tickers']:
     sp500_constituents = sp500_constituents | set(years_constituents)
